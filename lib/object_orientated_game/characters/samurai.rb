8914d9a30457463d117f5c062cb7739709edd0d9
jonathonyamada <jonathon.yamada@gmail.com>
2017-11-19 19:14:49 +0000

    Created samurai class constructor

--- lib/object_orientated_game/characters/samurai.rb
+++ lib/object_orientated_game/characters/samurai.rb
@@ -1,2 +1,8 @@
 class Samurai < Character
+  attr_reader :type
+
+  def initialize(name, health_points, attack_points, type)
+    super(name, health_points, attack_points)
+    @type = self.class
+  end
 end
