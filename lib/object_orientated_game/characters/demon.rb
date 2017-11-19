f07cce825a3bb90a805317b0b9c40d54936b8649
jonathonyamada <jonathon.yamada@gmail.com>
2017-11-19 19:13:06 +0000

    Created demon class constructor

--- lib/object_orientated_game/characters/demon.rb
+++ lib/object_orientated_game/characters/demon.rb
@@ -1,2 +1,8 @@
 class Demon < Character
+  attr_reader :type
+
+  def initialize(name, health_points, attack_points, type)
+    super(name, health_points, attack_points)
+    @type = self.class
+  end
 end
