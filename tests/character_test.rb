1523967f7fbea030677eb7b2c81faa707ca67801
jonathonyamada <jonathon.yamada@gmail.com>
2017-11-19 20:22:49 +0000

    Tested that show_player_selection method functions correctly

--- tests/character_test.rb
+++ tests/character_test.rb
@@ -5,6 +5,7 @@ require_relative '../lib/object_orientated_game/characters/ninja'
 class CharacterTest < Test::Unit::TestCase
   def setup
     @ninja = Ninja.new('Mr. Ninja', 100, 100, self.class)
+    @samurai = Samurai.new('Mr. Samurai', 200, 200, self.class)
   end
 
   test 'that show_full_stats method works with correct stdout formatting for ninja' do
@@ -19,5 +20,10 @@ class CharacterTest < Test::Unit::TestCase
     END
 
     assert_equal expected, @ninja.show_full_stats
+  end
+
+  test 'that show_player_selection functions as expected' do
+    assert_equal 'You chose Mr. Ninja the Ninja', @ninja.show_player_selection
+    assert_equal 'You chose Mr. Samurai the Samurai', @samurai.show_player_selection
   end
 end
