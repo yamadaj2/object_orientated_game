fe13714f5a9ec260cffc017aa0d563c03eb5f466
jonathonyamada <jonathon.yamada@gmail.com>
2017-11-20 12:08:45 +0000

    Created TrashTalk Module

---
+++ lib/object_orientated_game/modules/trash_talk.rb
@@ -0,0 +1,12 @@
+module TrashTalk
+  Lines =
+      [
+          "\"Where did you get those clothes? At the toilet store?\"",
+          "\"When I get a hold of you, I'm going to shove a sausage down your throat and stick starving dogs in your butt!\"",
+          "\"When I get a hold of you, I'm going to gouge out your eyes and shove 'em down your pants so you can watch me kick the crap out of you!\"",
+          "\"Do you feel lucky? Punk!\"",
+          "\"Sticks and stones may break my bones but I'm going to kick you repeatedly in the balls\"",
+          "\"Hi, I'm a stupid moron with an ugly face and a big butt and my butt smells and...I like to kiss my own butt.\"",
+          "\"It's me, Mr. Crowbar. And this is my friend who is also a crowbar.\""
+      ]
+end
