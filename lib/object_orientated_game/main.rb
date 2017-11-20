46e23bbf34e0a831550fad915dbc0231c8000a7b
jonathonyamada <jonathon.yamada@gmail.com>
2017-11-20 12:20:45 +0000

    added introduction scenes to main.rb

--- lib/object_orientated_game/main.rb
+++ lib/object_orientated_game/main.rb
@@ -7,3 +7,10 @@ gameplay = Gameplay.new
 player_character = gameplay.player_setup
 enemy_character = gameplay.random_enemy_selection
 puts player_character.show_player_selection
+
+# Introduction Scenes
+enemy_character.show_demon_intro_scenes if enemy_character.class == Demon
+puts enemy_character.show_challenge_line
+sleep 2
+puts enemy_character.show_character_intro unless self.class == Demon
+sleep 2
