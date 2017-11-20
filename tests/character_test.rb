51d215376089ca763417e1b9270e1d8d17abdb0b
jonathonyamada <jonathon.yamada@gmail.com>
2017-11-20 12:06:12 +0000

    Created test for show_challenge method

--- tests/character_test.rb
+++ tests/character_test.rb
@@ -2,12 +2,14 @@ require 'test/unit'
 require_relative '../lib/object_orientated_game/characters/character'
 require_relative '../lib/object_orientated_game/characters/ninja'
 require_relative '../lib/object_orientated_game/characters/samurai'
+require_relative '../lib/object_orientated_game/characters/demon'
 
 
 class CharacterTest < Test::Unit::TestCase
   def setup
     @ninja = Ninja.new('Mr. Ninja', 100, 100, self.class)
     @samurai = Samurai.new('Mr. Samurai', 200, 200, self.class)
+    @demon = Demon.new('Mr. Demon', 300, 300, self.class)
   end
 
   test 'that show_full_stats method works with correct stdout formatting for ninja' do
@@ -27,5 +29,11 @@ class CharacterTest < Test::Unit::TestCase
   test 'that show_player_selection functions as expected' do
     assert_equal 'You chose Mr. Ninja the Ninja', @ninja.show_player_selection
     assert_equal 'You chose Mr. Samurai the Samurai', @samurai.show_player_selection
+  end
+
+  test 'that show_challenge_line functions as expected' do
+    assert_equal 'Mr. Ninja the Ninja has appeared and is challenging you to a fight to the death!', @ninja.show_challenge_line
+    assert_equal 'Mr. Samurai the Samurai has appeared and is challenging you to a fight to the death!', @samurai.show_challenge_line
+    assert_equal 'Mr. Demon the Demon has appeared and is challenging you to a fight to the death!', @demon.show_challenge_line
   end
 end
