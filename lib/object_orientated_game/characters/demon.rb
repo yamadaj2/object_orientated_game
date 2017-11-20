ced0f223fbc7f4e3fec830ba18264be38528f487
jonathonyamada <jonathon.yamada@gmail.com>
2017-11-20 12:14:13 +0000

    Created custom Demon intro scenes

--- lib/object_orientated_game/characters/demon.rb
+++ lib/object_orientated_game/characters/demon.rb
@@ -7,4 +7,20 @@ class Demon < Character
     super(name, health_points, attack_points)
     @type = self.class
   end
+
+  def show_demon_intro_scenes
+    sleep 3
+    puts self.show_demon_boss_intro_scene
+    sleep 3
+    puts self.show_demon_intro_line
+    sleep 3
+  end
+
+  def show_demon_boss_intro_scene
+    EnemyList::ENEMY_LIST[:boss_demon][:scene]
+  end
+
+  def show_demon_intro_line
+    EnemyList::ENEMY_LIST[:boss_demon][:intro_line]
+  end
 end
